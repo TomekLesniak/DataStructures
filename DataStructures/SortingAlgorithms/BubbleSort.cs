2ac59876cfa7e166ac71cc88ca1c72ea1d103613
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-09-20 21:32:41 +0200

    SelectionSort alghorithm

--- DataStructures/SortingAlgorithms/BubbleSort.cs
+++ DataStructures/SortingAlgorithms/BubbleSort.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructures.SortingAlgorithms
 {
+
     public class BubbleSort
     {
         public static void Sort(int[] numbers)
---
+++ DataStructures/SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.SortingAlgorithms
+{
+    public class SelectionSort
+    {
+        public static void Sort(int[] numbers)
+        {
+            var currentSorted = 0;
+
+            while (currentSorted != numbers.Length)
+            {
+                var smallestIndex = GetIndexOfSmallest(numbers, currentSorted);
+                Swap(numbers, smallestIndex, currentSorted);
+                currentSorted++;
+            }
+        }
+
+        private static void Swap(int[] numbers, int indexFirst, int indexSecond)
+        {
+            int temp = numbers[indexFirst];
+            numbers[indexFirst] = numbers[indexSecond];
+            numbers[indexSecond] = temp;
+        }
+
+        private static int GetIndexOfSmallest(int[] numbers, int currentSmallest)
+        {
+            var smallestIndex = currentSmallest;
+
+            for (int i = currentSmallest; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[smallestIndex])
+                    smallestIndex = i;
+            }
+
+            return smallestIndex;
+        }
+    }
+}
